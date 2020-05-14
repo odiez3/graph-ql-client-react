f245ac2ea4902bd946c674193c21f5ce1debb74f
Oscar Diez <oscar.diez@gnp.com.mx>
2020-05-13 21:04:18 -0500

    Se eliminan configracion de firebase

--- src/properties/index.js
+++ src/properties/index.js
@@ -1,12 +1,6 @@
 
 export const FIREBASE_PROPS = {
-    apiKey: "AIzaSyDn52lGlpd0fIJmb8dpGCphYhOSVtYPfhs",
-    authDomain: "freeloader-d4303.firebaseapp.com",
-    databaseURL: "https://freeloader-d4303.firebaseio.com",
-    projectId: "freeloader-d4303",
-    storageBucket: "freeloader-d4303.appspot.com",
-    messagingSenderId: "139539714131",
-    appId: "1:139539714131:web:cf4c02461bd730faec36a6"
+   
   }
 
 export const GRAPH_URL = 'http://localhost:5000/graphql';
